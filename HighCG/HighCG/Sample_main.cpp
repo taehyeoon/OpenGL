d7488656d4f2872c20c73a4a2812fd560a973fd3
DESKTOP-74GI353\tedxp <tedxpem@naver.com>
2023-09-14 09:25:18 +0900

    Feat: Draw continuous line by mouse input

--- HighCG/HighCG/Sample_main.cpp
+++ HighCG/HighCG/Sample_main.cpp
@@ -11,11 +11,12 @@
 #include <GL/glut.h>
 
 using namespace std;
-list<float> verticesList;
-float* staticList;
 int mouseClicked;
 int screenWidth = 480;
 int screenHeight = 480;
+
+float positions[100];
+int VertexNum = 0;
 
 GLuint LoadShaders(const char* vertex_file_path, const char* fragment_file_path)
 {
@@ -94,14 +95,20 @@ GLuint LoadShaders(const char* vertex_file_path, const char* fragment_file_path)
     return ProgramID;
 }
 
-
 void renderScene(void)
 {
 	//Clear all pixels
 	glClear(GL_COLOR_BUFFER_BIT);
 	//Let's draw something here
 
-	glDrawElements(GL_TRIANGLES, verticesList.size(), GL_UNSIGNED_INT, nullptr);
+	GLuint VBO;
+	glGenVertexArrays(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glDrawArrays(GL_LINE_STRIP, 0, VertexNum);
 	//define the size of point and draw a point.
 	//glDrawArrays(GL_TRIANGLES, 0, 3);
 
@@ -113,7 +120,6 @@ void renderScene(void)
 void init()
 {
     //initilize the glew and check the errors.
-    
     GLenum res = glewInit();
     if(res != GLEW_OK)
     {
@@ -125,25 +131,6 @@ void init()
 	glEnable(GL_VERTEX_PROGRAM_POINT_SIZE);
 }
 
-void dynamic2staticList() {
-	// 리스트의 크기를 알아야 합니다.
-	size_t size = verticesList.size();
-
-	if (staticList != nullptr)
-		delete[] staticList;
-
-	// 동적으로 할당된 배열을 생성합니다.
-	float* resultArray = new float[size];
-
-	// 리스트의 요소를 배열에 복사합니다.
-	size_t index = 0;
-	for (const float& value : verticesList) {
-		resultArray[index] = value;
-		++index;
-	}
-
-	staticList = resultArray;
-}
 
 pair<float, float> nomalizedPosByLeftTop(int x, int y) {
 	pair<float, float> result;
@@ -160,16 +147,20 @@ void mousePressed(int btn, int state, int x, int y) {
 		pair<float, float> normalPos = nomalizedPosByLeftTop(x, y);
 		cout << normalPos.first << " , " << normalPos.second << "\n" << endl;
 
-		verticesList.push_back(normalPos.first);
-		verticesList.push_back(normalPos.second);
-
-		dynamic2staticList();
+		positions[VertexNum*2] = normalPos.first;
+		positions[VertexNum*2+1] = normalPos.second;
+		VertexNum++;
 	}
 	else {
 		mouseClicked = false;
 	}
 }
 
+void keyboardPressed(unsigned char key, int x, int y)
+{
+	if (key == 27)
+		exit(0);
+}
 
 int main(int argc, char **argv)
 {
@@ -187,68 +178,26 @@ int main(int argc, char **argv)
 	//call initization function
 	init();
 
-	float positions[] = {
-		-0.4f,  1.0f, // 0
-		-1.0f,  0.0f, // 1
-		0.0f, -0.4f, // 2
-		-1.0f, -0.4f, // 3
-		-0.7f, -1.0f, // 4
-		-0.4f, -0.4f, // 5
-		0.0f, -0.7f, // 6
-		0.4f, -1.0f, // 7
-		1.0f,  0.0f, // 8
-		0.0f,  0.4f, // 9
-		1.0f,  0.4f, // 10
-		1.0f,  0.7f  // 11
-	};
-
-	for (float val : positions) {
-		verticesList.push_back(val);
-	}
-
-	unsigned int indices[] = {
-		0, 1, 2, // t1
-		3, 4, 5,  // t2
-		6, 7, 8, // t3
-		9, 10, 11  // t4
-	};
+	//float positions[] = {
+	//	-0.2f, -0.8f,
+	//	0.8f, 0.2f,
+	//};
 
 	glutMouseFunc(mousePressed);
+	glutKeyboardFunc(keyboardPressed);
 
 	//1.
 	//Generate VAO
-	GLuint VertexArrayID;
-	glGenVertexArrays(1, &VertexArrayID);
-	glBindVertexArray(VertexArrayID);
+	GLuint VAO;
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
 
-
-	dynamic2staticList();
-	GLuint bufferID;
-	glGenBuffers(1, &bufferID);
-	glBindBuffer(GL_ARRAY_BUFFER, bufferID); // <-- bind 는 activate 역할
-	glBufferData(GL_ARRAY_BUFFER, // 실제 data를 CPU -> GPU 로 넘기는 과정
-				verticesList.size() * 2 * sizeof(float),
-				staticList,
-				GL_STATIC_DRAW);
-	
-	glEnableVertexAttribArray(0); // shader 관련, 0번 location에 저장할거다라는 의미
-	glVertexAttribPointer(0, // 0번 location에 있는 데이터들을 읽기 시작할거다라는 의미
-						2, // 하나의 vertex에 몇개의 데이터를 넘기는지, 여기서는 x,y이므로 2개
-						GL_FLOAT, // 입력해주는 데이터 타입
-						GL_FALSE,
-						sizeof(float) * 2, // 값을 하나 읽을 때마다, 몇 바이트 뒤의 데이터를 읽을 지
-						0); // 0번째 데이터부터 읽기 시작한다
-
-	GLuint vboID;
-	glGenBuffers(1, &vboID);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vboID);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, verticesList.size() * sizeof(GLuint), indices, GL_STATIC_DRAW);
-	
-
-	
-
-
-	
+	GLuint VBO;
+	glGenVertexArrays(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, (void*)0);
+	glEnableVertexAttribArray(0);
 
 	//3. 
 	GLuint programID = LoadShaders("VertexShader.txt", "FragmentShader.txt");
@@ -260,7 +209,7 @@ int main(int argc, char **argv)
 	//enter GLUT event processing cycle
 	glutMainLoop();
 
-	glDeleteVertexArrays(1, &VertexArrayID);
+	glDeleteVertexArrays(1, &VAO);
 
 	return 1;
 }
